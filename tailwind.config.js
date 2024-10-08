ec15c237e8d639dc1fa0de6763f6ce3bd27f3364
Rohit <theurkarrohit991@gmail.com>
2024-10-08 12:11:06 +0530

    Build added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   darkMode: "class", // Enables dark mode using a 'class' strategy
   purge: [
-    "./src/*/.{js,jsx,ts,tsx}",  // Correct path to include all JS, JSX, TS, and TSX files
+    "./src/**/*.{js,jsx,ts,tsx}",  // Correct path to include all JS, JSX, TS, and TSX files
     "./public/index.html",         // Include the HTML file(s) in your project
   ],
   theme: {
